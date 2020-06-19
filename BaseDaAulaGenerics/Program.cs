5f5199aeba362e3e7a24cea91024422f4bcc1dd9
Rebeca Buarque <rebecabuarque@gmail.com>
2020-06-19 16:18:19 +0100

    First Generic Type Implementation

---
+++ BaseDaAulaGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BaseDaAulaGenerics.Service;
+namespace BaseDaAulaGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>(); 
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.addValue(x);
+            }
+
+            printService.print();
+            Console.WriteLine();
+            Console.WriteLine("First: " + printService.first());
+
+        }
+    }
+}
